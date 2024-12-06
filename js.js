0e878eda4e4d335c0dcdee1bcd2733e23488a85d
mahdiija <mahdijalinousi1@gmail.com>
2024-12-06 21:55:24 +0330

    second

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 const sidebarItems = document.querySelectorAll(".sidebar li");
 const contentSections = document.querySelectorAll(".content");
 const themeSwitcher = document.getElementById("theme-switcher");
-const themeIcon = document.getElementById("theme-icon"); // Select the image element for the icon
+const themeIcon = document.getElementById("theme-icon");
 const languageSwitcher = document.getElementById("language-switcher");
 
 const icons = [
@@ -17,66 +17,132 @@ const icons = [
   { name: "Ts", file: "frame9.svg" },
 ];
 const Projects = [
-    { name: "1", file: "Thumb.png", link:"http://127.0.0.1:5500/index.html ",description: `it is a beauty website that i am working on it and it is nearly done. 
-        <br>it is called ZibatoShop <br>`},
-    { name: "2", file: "Wsa.png", link:"https://wsamasters.acetech.ir/",description: `It is a Site for Mini football Events and it is called Wsa Masters <br><br> !Click on it to go to the website!` },
-    { name: "3", file: "austria.png", link:"https://austriatour.acetech.ir/",description: `It is a Site for Mini football Events and it is called Austria Tour <br><br> !Click on it to go to the website!` },
-    { name: "4", file: "socca.png", link:"https://soccaaustria.com/en",description: `We developed leauges in this site and Make overlays for streams of the matches Have panels for Players, referee and teams! <br><br> !Click on it to go to the website!` },
-    { name: "5", file: "setare1.png", link:"https://setareyek.ir/",description: `I was a small part of this Pwa app and done some of the car service part of this App <br><br> !Click on it to go to the website to download the App!` },
-
+  {
+    name: "1",
+    file: "Thumb.png",
+    link: "http://127.0.0.1:5500/index.html ",
+    description: `it is a beauty website that i am working on it and it is nearly done. 
+        <br>it is called ZibatoShop <br>`,
+  },
+  {
+    name: "2",
+    file: "Wsa.png",
+    link: "https://wsamasters.acetech.ir/",
+    description: `It is a Site for Mini football Events and it is called Wsa Masters <br><br> !Click on it to go to the website!`,
+  },
+  {
+    name: "3",
+    file: "austria.png",
+    link: "https://austriatour.acetech.ir/",
+    description: `It is a Site for Mini football Events and it is called Austria Tour <br><br> !Click on it to go to the website!`,
+  },
+  {
+    name: "4",
+    file: "socca.png",
+    link: "https://soccaaustria.com/en",
+    description: `We developed leauges in this site and Make overlays for streams of the matches Have panels for Players, referee and teams! <br><br> !Click on it to go to the website!`,
+  },
+  {
+    name: "5",
+    file: "setare1.png",
+    link: "https://setareyek.ir/",
+    description: `I was a small part of this Pwa app and done some of the car service part of this App <br><br> !Click on it to go to the website to download the App!`,
+  },
 ];
 const iconContainer = document.querySelector(".icon-container");
 const ProjectsContainer = document.querySelector(".project-container");
 
-// Function to create and add icons dynamically
 function displayIcons() {
   icons.forEach((icon) => {
     const img = document.createElement("img");
-    img.src = `icons/${icon.file}`; // Set the SVG icon source
-    img.alt = `${icon.name} icon`; // Add alt text
-    img.width = 60; // Set the icon size (you can customize this)
-    img.height = 60; // Set the icon size (you can customize this)
-    img.classList.add("icon"); // Add class for styling (optional)
+    img.src = `icons/${icon.file}`;
+    img.alt = `${icon.name} icon`;
+    img.width = 60;
+    img.height = 60;
+    img.classList.add("icon");
 
-    // Append the icon to the container
     iconContainer.appendChild(img);
   });
 }
 
-// Call the function to display the icons when the page loads
 document.addEventListener("DOMContentLoaded", displayIcons);
 
-
 function displayProject() {
-    Projects.forEach((icon) => {
-
-
-       const anchor = document.createElement('a');
-    anchor.href = icon.link; // Set the href to the external URL
-    anchor.target = '_blank'; 
-    anchor.className = 'icon-link';
+  Projects.forEach((icon) => {
+    const anchor = document.createElement("a");
+    anchor.href = icon.link;
+    anchor.target = "_blank";
+    anchor.className = "icon-link";
 
     const img = document.createElement("img");
-    img.src = `images/${icon.file}`; // Set the SVG icon source
-    img.alt = `${icon.name} icon`; // Add alt text
-    img.width = 370; // Set the icon size (you can customize this)
-    img.height = 302; // Set the icon size (you can customize this)
-    img.classList.add("project"); // Add class for styling (optional)
-    const description = document.createElement('span');
-    description.className = 'description';
+    img.src = `images/${icon.file}`;
+    img.alt = `${icon.name} icon`;
+    img.width = 370;
+    img.height = 302;
+    img.classList.add("project");
+    const description = document.createElement("span");
+    description.className = "description";
     description.innerHTML = icon.description;
-    // Append the icon to the container
+
     anchor.appendChild(img);
     anchor.appendChild(description);
-    // Append the anchor (with the icon inside) to the container
+
     ProjectsContainer.appendChild(anchor);
   });
 }
 
-// Call the function to display the icons when the page loads
 document.addEventListener("DOMContentLoaded", displayProject);
+const menuToggle = document.getElementById("menu-toggle");
+const closeSidebarButton = document.getElementById("close-sidebar");
+const sidebar = document.querySelector(".sidebar");
+const navItems = document.querySelectorAll(".sidebar ul li");
 
-// Function to activate a specific page
+const isMobile = () => window.innerWidth <= 768;
+
+menuToggle.addEventListener("click", () => {
+  if (isMobile()) {
+    sidebar.classList.add("visible");
+    sidebar.classList.remove("hidden");
+  }
+});
+
+closeSidebarButton.addEventListener("click", () => {
+  if (isMobile()) {
+    sidebar.classList.remove("visible");
+    sidebar.classList.add("hidden");
+  }
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    isMobile() &&
+    !sidebar.contains(event.target) &&
+    !menuToggle.contains(event.target)
+  ) {
+    sidebar.classList.remove("visible");
+    sidebar.classList.add("hidden");
+  }
+});
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (isMobile()) {
+      sidebar.classList.remove("visible");
+      sidebar.classList.add("hidden");
+    }
+  });
+});
+
+window.addEventListener("resize", () => {
+  if (!isMobile()) {
+    sidebar.classList.remove("hidden");
+    sidebar.classList.add("visible");
+  } else {
+    sidebar.classList.remove("visible");
+    sidebar.classList.add("hidden");
+  }
+});
+
 function activatePage(pageId) {
   sidebarItems.forEach((i) => i.classList.remove("active"));
   contentSections.forEach((section) => section.classList.remove("active"));
@@ -92,51 +158,42 @@ function activatePage(pageId) {
   }
 }
 function switchLanguage(lang) {
-  // Update the body class to switch languages (EN/FA)
   document.body.classList.remove("en", "fa");
   document.body.classList.add(lang);
 
-  // Save the selected language in localStorage
   localStorage.setItem("language", lang);
 
-  // Update the language switcher button text
   languageSwitcher.textContent = lang === "fa" ? "FA" : "EN ";
 }
 document.addEventListener("DOMContentLoaded", () => {
-  // Get saved language from localStorage, default to English (en)
   const savedLanguage = localStorage.getItem("language") || "en";
   switchLanguage(savedLanguage);
 
-  // Check for the theme (dark or light) from localStorage and apply
   const savedTheme = localStorage.getItem("theme") || "light";
   if (savedTheme === "dark") {
     document.body.classList.add("dark-mode");
-    themeIcon.src = "icons/moon-icon.svg"; // Change to moon icon
+    themeIcon.src = "icons/moon-icon.svg";
   }
 });
 
-// Add click event listener to the language switcher button
 languageSwitcher.addEventListener("click", () => {
   const currentLang = document.body.classList.contains("fa") ? "fa" : "en";
   const newLang = currentLang === "en" ? "fa" : "en";
   switchLanguage(newLang);
 });
 
-// On page load, check localStorage for the active page and theme
 document.addEventListener("DOMContentLoaded", () => {
   const savedPage = localStorage.getItem("activePage") || "home";
   const savedTheme = localStorage.getItem("theme") || "light";
 
-  // Set theme on load
   if (savedTheme === "dark") {
     document.body.classList.add("dark-mode");
-    themeIcon.src = "icons/moon-icon.svg"; // Switch to moon icon
+    themeIcon.src = "icons/moon-icon.svg";
   }
 
   activatePage(savedPage);
 });
 
-// Add click event listener to each sidebar item
 sidebarItems.forEach((item) => {
   item.addEventListener("click", () => {
     const pageId = item.getAttribute("data-page");
@@ -145,32 +202,24 @@ sidebarItems.forEach((item) => {
   });
 });
 
-// Theme toggle functionality
 themeSwitcher.addEventListener("click", () => {
   const isDarkMode = document.body.classList.toggle("dark-mode");
 
-  // Update the icon based on the theme
   if (isDarkMode) {
     themeIcon.src = "icons/moon-icon.svg";
   } else {
     themeIcon.src = "icons/sun-icon.svg";
   }
 
-  // Save the theme in localStorage
   localStorage.setItem("theme", isDarkMode ? "dark" : "light");
 });
 
-
-
 function toggleCard(card) {
-  // Toggle the 'expanded' class on the card
   card.classList.toggle("expanded");
 }
 function toggleCard1(card1) {
-  // Toggle the 'expanded' class on the card
   card1.classList.toggle("expanded1");
 }
 function toggleCard2(card2) {
-
   card2.classList.toggle("expanded2");
 }
